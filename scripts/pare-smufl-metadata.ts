4eba639c2cefe93b6eb1113d913cbc014f774e7e
Diane Nguyen <me@definitely-diane.com>
2025-12-04 19:52:03 -0800

    Tree shake font metadata

--- scripts/pare-smufl-metadata.ts
+++ scripts/pare-smufl-metadata.ts
@@ -4,13 +4,15 @@
 //
 
 import GlyphNameMeta from "../server/smufl-metadata/glyphnames.json" with { type: "json" };
-import GlyphClass from "../server/smufl-metadata/classes.json" with { type: "json" };
+import GlyphClasses from "../server/smufl-metadata/classes.json" with { type: "json" };
 import BravuraMeta from "../server/bravura_metadata.json" with { type: "json" };
 
 if (import.meta.main) {
     // We can scan each file in each source directory for the `getGlyph()` method call
     // We can also search for unicode and hexcode strings for this
-    const fnames= ["./server/SVGGenerator.ts"];
+    const fnames = await getFilePaths();
+		console.log("Scanning files:");
+		fnames.forEach(v => console.log(v));
 
     const glyphNames = new Set();
 
@@ -28,5 +30,93 @@ if (import.meta.main) {
         glyphNamesOutput[gname] = GlyphNameMeta[gname];
     }
 
-    await Deno.writeTextFile("ParedMetadataTest.json", JSON.stringify(glyphNamesOutput));
+
+    await Deno.writeTextFile("public/fonts/metadata/glyphnames.json", JSON.stringify(glyphNamesOutput));
+		console.log("Glyph names written to: public/fonts/metadata/glyphnames.json");
+
+		const classes = extractClassMeta({glyphNames});
+		await Deno.writeTextFile("public/fonts/metadata/classes.json", JSON.stringify(classes));
+		console.log("Classes written to: public/fonts/metadata/classes.json");
+
+		const fontMeta = extractFontMeta(glyphNames);
+		await Deno.writeTextFile("public/fonts/metadata/bravura_metadata.json", JSON.stringify(fontMeta));
+		console.log("Font meta written to: public/fonts/metadata/bravura_metadata.json");
+}
+
+async function getFilePaths({ignoreDirs, ignoreFiles}: {
+	ignoreDirs: string[],
+	ignoreFiles: string[]
+} = { ignoreDirs: [".git"], ignoreFiles: [] }): string[] {
+	const dirs = ["."];
+	const ignoreDirSet = new Set(ignoreDirs);
+	const results = [];
+	const re = /\.(\/[A-Za-z\-0-9\.]+)+.(ts|js)(?!.+)/;
+	while(dirs.length) {
+		const dir = dirs.shift();
+		for await (const record of Deno.readDir(dir)) {
+			const dpath = `${dir}/${record.name}`;
+			if(record.isDirectory) {
+				// Remove the leading ./ from the path string
+				if(!ignoreDirSet.has(dpath.slice(2))){
+				 	dirs.push(dpath);
+				}
+			} else if(record.isFile && re.test(dpath)) {
+				results.push(dpath);
+			}
+		}
+	}
+	return results;
+}
+
+function extractClassMeta({glyphNames, ignoreClasses }: {
+	glyphNames: Set<string>,
+	ignoreClasses?: Set<string>}
+):  Object {
+	const result = {};
+
+	for(const key in GlyphClasses) {
+		const glyphs = new Set(GlyphClasses[key]);
+		const intersection = glyphNames.intersection(glyphs);
+		if(intersection.size) {
+			result[key] = Array.from(intersection.values());
+		}
+	}
+
+	return result;
+}
+
+function extractFontMeta(glyphNames: Set<string>): Object {
+	const result = {};
+
+	// Handles all properties where the glyph is used as a key.
+	// E.g. glyphAdvanceWidths, glyphsWithAlternates, glyphsWithAnchors, etc.
+	const handleGlyphAsKey = (key: string) => {
+		const obj = BravuraMeta[key];
+		const keySet = new Set(Object.keys(obj));
+		const intersection = glyphNames.intersection(keySet);
+		if(!intersection.size) return;
+		result[key] = {};
+		for(const k of intersection.values()) {
+			result[key][k] = obj[k];
+		}
+	}
+
+	for(const key in BravuraMeta) {
+		switch(key) {
+			case "fontName":
+			case "fontVersion":
+			case "engravingDefaults":
+				result[key] = BravuraMeta[key];
+			case "glyphAdvanceWidths":
+			case "glyphBBoxes":
+			case "glyphsWithAlternates":
+			case "glyphsWithAnchors":
+			case "ligatures":
+			case "optionalGlyphs":
+				handleGlyphAsKey(key);
+			// TODO: sets?
+		}
+	}
+
+	return result;
 }
